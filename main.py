f9e79e5529a8da0a31d19834f782f4a4bf85f26a
Alex <e9285522@gmail.com>
2026-05-19 09:29:27 -0400

    add code-check bot

--- main.py
+++ main.py
@@ -9,3 +9,11 @@ def afficher_menu():
     print("3. Modifier un livre")
     print("4. Supprimer un livre")
     print("5. Quitter")
+def afficher_livres():
+    for l in livre:
+        print(l)
+def ajouter_livre():
+    nom = input("Entrez le nom du livre à ajouter: ")
+    livre.append(nom)
+    print(f"Le livre '{nom}' a été ajouté.")
+def modifier_livre():
