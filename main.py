93da1eae0715e36ebdbba68f1b26263c159c9fcb
Alex <e9285522@gmail.com>
2026-05-19 09:15:04 -0400

    creation menu

---
+++ main.py
@@ -0,0 +1,11 @@
+#application de gestion de livre
+
+livre = ["merlin","Voiture", "Arthur"]
+
+def afficher_menu():
+    print("\n==== MENU PRINCIPAL ====")
+    print("1. Afficher les livres")
+    print("2. Ajouter un livre")
+    print("3. Modifier un livre")
+    print("4. Supprimer un livre")
+    print("5. Quitter")
